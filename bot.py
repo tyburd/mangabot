f02a14458a2f231cd70666c7d7064fba0955ef0f
Yi-Zaha <136574884+Yi-Zaha@users.noreply.github.com>
2023-06-27 10:19:59 +0530

    k

--- bot.py
+++ bot.py
@@ -811,9 +811,9 @@ async def update_mangas():
                     continue
                 try:
                     try:
-                        await chapter_click(bot, chapter.unique(), int(sub), custom_caption=custom_caption, custom_filename)
+                        await chapter_click(bot, chapter.unique(), int(sub), custom_caption=custom_caption, custom_filename=custom_filename)
                     except BaseException:
-                        await chapter_click(bot, chapter.unique(), int(sub), custom_caption=custom_caption, custom_filename)
+                        await chapter_click(bot, chapter.unique(), int(sub), custom_caption=custom_caption, custom_filename=custom_filename)
                 except pyrogram.errors.UserIsBlocked:
                     print(f'User {sub} blocked the bot')
                     await remove_subscriptions(sub)
